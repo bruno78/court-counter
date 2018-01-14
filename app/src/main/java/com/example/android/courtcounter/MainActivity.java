0f982343d522ba818428986a243a2ffd3040c6d9
bruno78 <devbruno78@gmail.com>
2018-01-14 00:12:59 -0500

    Add methods for scores and reset

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,59 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(scoreA);
     }
+
+    /**
+     * Display the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Display the given score for Team A.
+     */
+    public void addThree(View view) {
+        scoreA += 3;
+        displayForTeamA(scoreA);
+
+    }
+
+    /**
+     * Display the given score for Team A.
+     */
+    public void addTwo(View view) {
+        scoreA += 2;
+        displayForTeamA(scoreA);
+
+    }
+
+    /**
+     * Display the given score for Team A.
+     */
+    public void addFreeThrow(View view) {
+        scoreA += 1;
+        displayForTeamA(scoreA);
+
+    }
+
+    public void reset(View view) {
+        scoreA = 0;
+        displayForTeamA(scoreA);
+    }
+
+
 }
